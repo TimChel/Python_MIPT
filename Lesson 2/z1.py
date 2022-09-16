31f6370496d66826c3d41904988b86b457d48262
TimChel <timchelyadinsky@yandex.by>
2022-09-16 15:05:39 +0300

    Обнова 1
    Чуть не сломал комп. Не просить посчитать n=100000

---
+++ Lesson 2/z1.py
@@ -0,0 +1,8 @@
+print('Прив! Это программа высчитываен n-ое число Фибоначчи. Введите n:')
+n = int(input())
+fib = []
+fib.append(1)
+fib.append(1)
+for i in range(2, n):
+    fib.append(fib[i-1] + fib[i-2])
+print('n-ое число Фибоначчи: ', fib[-1])
